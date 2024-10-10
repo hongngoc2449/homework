9e421382de57bdffbcc85505fb1b96a5c1b6a631
hongngocdoan <hongngoc2449@gmail.com>
2024-10-10 18:19:48 +0700

    hw5

---
+++ homework5/arrowFunction.js
@@ -0,0 +1,6 @@
+const perimeter = (width,height) => 2 * (width + height);
+const area = (width,height) => width * height;
+const width = 5;
+const height = 10;
+console.log("Chu vi hinh chu nhat la: " + perimeter(width,height));
+console.log("Dien tich hinh chu nhat la: " + area(width,height));
