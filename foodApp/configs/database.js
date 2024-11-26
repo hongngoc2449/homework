d5ce918c4b006301293ff846fd06e2c38de226c9
hongngocdoan <hongngoc2449@gmail.com>
2024-11-26 18:36:19 +0700

    CRUD Food

--- foodApp/configs/database.js
+++ foodApp/configs/database.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");						
-						
-async function connectDB(){						
-try {						
-await mongoose.connect("mongodb://localhost:27017/food-order");						
-console.log("connect db success");						
-} catch(error){						
-console.log("connect db fail: ", error.message);						
-}						
-}						
-						
-module.exports = connectDB;						
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+async function connectDB(){
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("connect db success");
+    } catch(error){
+        console.log("connect db fail: ", error.message);
+    }
+}
+
+module.exports = connectDB;
--- foodApp/models/category.model.js
+++ foodApp/models/category.model.js
@@ -1,8 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const categorySchema = mongoose.Schema({
-    name: {type: String, required: true, unique: true},
-    img: {type: String, required: true}
-})
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    img: {
+        type: String,
+        required: true
+    }
+});
 
-module.exports = mongoose.model('category', categorySchema)
+module.exports = mongoose.model('Category', categorySchema);
