16df828981ed15cdc5547a8913c974d20c58b658
hongngocdoan <hongngoc2449@gmail.com>
2024-11-26 19:28:11 +0700

    product-api

--- expressMongodb/app.js
+++ expressMongodb/app.js
@@ -13,7 +13,7 @@ useNewUrlParser: true,
 useUnifiedTopology: true											
 })											
 .then(() => {											
-console.log('Connected to MongoDB');											
+console.log('Connected to MongoDB');					    						
 })											
 .catch(err => {											
 console.error('Failed to connect to MongoDB', err);											
